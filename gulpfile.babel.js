8f019f67cbfa153b48196001f26f6dfa5721760f
CGJI58 <leemsk22@naver.com>
2024-01-09 16:20:37 +0900

    build css done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,12 @@ const routes = {
     src: "./src/*.html",
     dest: "build",
   },
+  css: {
+    base: "./src/css/*.css",
+    components: "./src/css/components/*.css",
+    screens: "./src/css/screens/*.css",
+    dest: "build/css",
+  },
 };
 
 const html = () => gulp.src(routes.html.src).pipe(gulp.dest(routes.html.dest));
@@ -16,9 +22,16 @@ const clean = async () => await deleteAsync(["build/"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
+const styles = () =>
+  gulp
+    .src(routes.css.base)
+    .pipe(gulp.src(routes.css.components))
+    .pipe(gulp.src(routes.css.screens))
+    .pipe(gulp.dest(routes.css.dest));
+
 const prepare = gulp.series([clean]);
 
-const assets = gulp.series([html]);
+const assets = gulp.series([html, styles]);
 
 const postDev = gulp.series([webserver]);
 
